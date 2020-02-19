20c3055613e9b75395632287e5377400c677e971
Michiel Kodde <mkodde@ibuildings.nl>
2020-02-19 11:05:30 +0100

    Setup a Stepup namespace

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use StepupReadId\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App;
+namespace StepupReadId;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
