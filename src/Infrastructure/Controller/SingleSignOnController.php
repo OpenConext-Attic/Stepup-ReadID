e0d9a81d0d716468d12fd4404e1587637a7411e4
Michiel Kodde <mkodde@ibuildings.nl>
2020-02-26 15:10:35 +0100

    Install Stepup SAML bundle
    The installation includes the configuration of the bundle for IdP use.
    
    For now some todo's remain:
    1. Do not use the default cert+private key from the SAML bundle
    2. Move configuration parameters to a more easily overwritable
    paramteres.yaml file
    3. Provide some test coverage

---
+++ src/Infrastructure/Controller/SingleSignOnController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright 2017 SURFnet B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace StepupReadId\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class SingleSignOnController extends AbstractController
+{
+    /**
+     * @Route("/saml/sso", name="readid_saml_sso", methods={"GET"})
+     */
+    public function singleSignOnAction(): Response
+    {
+        return new Response();
+    }
+}
