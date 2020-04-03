62cd3375d4831a0eb4dcb551a83bfe8622714c6a
Sergio Gómez <sergio@uco.es>
2020-04-03 11:53:16 +0200

    Read configuration parameters from environment

--- src/Infrastructure/Services/HttpReadIDConfiguration.php
+++ src/Infrastructure/Services/HttpReadIDConfiguration.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace StepupReadId\Infrastructure\Services;
 
+use InvalidArgumentException;
+use function filter_var;
+use const FILTER_FLAG_HOSTNAME;
+use const FILTER_VALIDATE_DOMAIN;
+
 final class HttpReadIDConfiguration
 {
     /** @var string */
@@ -15,6 +20,10 @@ final class HttpReadIDConfiguration
 
     public function __construct(string $authorizationToken, string $opaqueId, string $readIdServerFqdn)
     {
+        $this->checkValidAuthorizationToken($authorizationToken);
+        $this->checkValidOpaqueId($opaqueId);
+        $this->checkValidHostName($readIdServerFqdn);
+
         $this->authorizationToken = $authorizationToken;
         $this->opaqueId           = $opaqueId;
         $this->readIdServerFqdn   = $readIdServerFqdn;
@@ -33,5 +42,26 @@ final class HttpReadIDConfiguration
     public function readIdServerFqdn(): string
     {
         return $this->readIdServerFqdn;
+    }
+
+    private function checkValidAuthorizationToken(string $authorizationToken): void
+    {
+        if (empty($authorizationToken)) {
+            throw new InvalidArgumentException('Authorization token can not be empty');
+        }
+    }
+
+    private function checkValidOpaqueId(string $opaqueId): void
+    {
+        if (empty($opaqueId)) {
+            throw new InvalidArgumentException('Opaque ID can not be empty');
+        }
+    }
+
+    private function checkValidHostName(string $readIdServerFqdn): void
+    {
+        if (!filter_var($readIdServerFqdn, FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)) {
+            throw new InvalidArgumentException("Invalid hostname: '$readIdServerFqdn'");
+        }
     }
 }
