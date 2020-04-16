ee1888131e860dce8fe4f6ef6dafad145a43e6ac
Sergio Gómez <sergio@uco.es>
2020-04-16 08:57:49 +0200

    Fix SessionNormalizer methods signatures
    - supportsNormalization has an extra parameter
    - added array return type to normalize method, this removes an error in phpstan

--- src/Infrastructure/Serializer/SessionNormalizer.php
+++ src/Infrastructure/Serializer/SessionNormalizer.php
@@ -5,17 +5,20 @@ declare(strict_types=1);
 namespace StepupReadId\Infrastructure\Serializer;
 
 use StepupReadId\Domain\Session\Model\Session;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 final class SessionNormalizer implements NormalizerInterface
 {
     /**
-     * @param Session $object
-     *
      * @inheritdoc
      */
-    public function normalize($object, $format = null, array $context = [])
+    public function normalize($object, $format = null, array $context = []): array
     {
+        if (!$object instanceof Session) {
+            throw new InvalidArgumentException();
+        }
+
         return [
             'id' => $object->id()->value(),
             'expiryDate' => $object->expiryDate()->value()->getTimestamp(),
@@ -26,7 +29,7 @@ final class SessionNormalizer implements NormalizerInterface
     /**
      * @inheritdoc
      */
-    public function supportsNormalization($data, $format = null, array $context = [])
+    public function supportsNormalization($data, $format = null)
     {
         return $data instanceof Session;
     }
