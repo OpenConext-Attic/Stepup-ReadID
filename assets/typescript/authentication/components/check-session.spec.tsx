5d7eb38c63c713bf25bdeec3572ceb0c75b949a0
Bas Strooband <bstrooband@ibuildings.nl>
2021-03-08 18:24:32 +0100

    Add frontend linting

--- assets/typescript/authentication/components/check-session.spec.tsx
+++ assets/typescript/authentication/components/check-session.spec.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import {render, screen, act, cleanup} from '@testing-library/react';
-import CheckSession  from './check-session';
-import VerifyAPI, {VerifyResponse} from "../app/api/verify-api";
+import { render, screen, act, cleanup } from '@testing-library/react';
+import CheckSession from './check-session';
+import VerifyAPI, { VerifyResponse } from '../app/api/verify-api';
 
-describe("Element should poll verification state", () => {
-
-  const counterTest = async (duration: number, retry: number) =>{
-
+describe('Element should poll verification state', () => {
+  const counterTest = async (duration: number, retry: number) => {
     // Test polling steps
-    const expectedValue = retry + '/2,' + duration + '/3';
-    const stateText = await screen.findByTestId("check-session-polling");
+    const expectedValue = `${retry}/2,${duration}/3`;
+    const stateText = await screen.findByTestId('check-session-polling');
     expect(stateText.textContent).toMatch(expectedValue);
 
     act(() => {
       jest.advanceTimersByTime(1000);
-    })
-  }
+    });
+  };
 
-  it("renders", async () => {
+  it('renders', async () => {
     render(
       <CheckSession
         verifyUrl="https://readid.stepup.example.com/verify"
@@ -25,24 +23,25 @@ describe("Element should poll verification state", () => {
         retryInSeconds={2}
         maxTries={3}
         timeOutMessage="Unable to process verification"
-      />,
+      />
     );
 
-    const stateText = await screen.findByTestId("check-session-polling");
-    expect(stateText.textContent).toMatch("0/2,0/3");
+    const stateText = await screen.findByTestId('check-session-polling');
+    expect(stateText.textContent).toMatch('0/2,0/3');
 
-     cleanup();
-  })
+    cleanup();
+  });
 
-  it("polls verify api when not confirmed", async () => {
-
+  it('polls verify api when not confirmed', async () => {
     jest.useFakeTimers();
 
     // Mock VerifyAPI
     jest.mock('../app/api/verify-api');
     const fetchStatus = jest.fn();
     fetchStatus.mockResolvedValue(
-      JSON.parse(`{"success": true,"payload":{"confirmed":false}}`) as VerifyResponse
+      JSON.parse(
+        `{"success": true,"payload":{"confirmed":false}}`
+      ) as VerifyResponse
     );
     VerifyAPI.prototype.fetchStatus = fetchStatus;
 
@@ -53,59 +52,51 @@ describe("Element should poll verification state", () => {
         retryInSeconds={2}
         maxTries={3}
         timeOutMessage="Unable to process verification"
-      />,
+      />
     );
 
     const tests = [
-      [0, 0], [0, 1],
-      [1, 0], [1, 1],
-      [2, 0], [2, 1],
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+      [2, 0],
+      [2, 1],
       [2, 2]
     ];
 
-    expect.assertions(tests.length+1);
+    expect.assertions(tests.length + 1);
 
-    const counterTest = async (duration: number, retry: number) =>{
-
-      // Test polling steps
-      const expectedValue = retry + '/2,' + duration + '/3';
-      const stateText = await screen.findByTestId("check-session-polling");
-      expect(stateText.textContent).toMatch(expectedValue);
-
-      act(() => {
-        jest.advanceTimersByTime(1000);
-      })
-    }
-
-    for (let i = 0; i < tests.length; i++) {
+    for (let i = 0; i < tests.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
       await counterTest(tests[i][0], tests[i][1]);
     }
 
     // Test final timeout
-    const expectedValue = "Unable to process verification";
+    const expectedValue = 'Unable to process verification';
     const stateText = await screen.findByText(expectedValue);
     expect(stateText.textContent).toMatch(expectedValue);
 
     cleanup();
-  })
+  });
 
-
-  it("redirects to authentication url when confirmed", async () => {
-
+  it('redirects to authentication url when confirmed', async () => {
     jest.useFakeTimers();
 
     // Mock VerifyAPI
     jest.mock('../app/api/verify-api');
     const fetchStatus = jest.fn();
     fetchStatus.mockResolvedValue(
-      JSON.parse(`{"success": true,"payload":{"confirmed":true}}`) as VerifyResponse
+      JSON.parse(
+        `{"success": true,"payload":{"confirmed":true}}`
+      ) as VerifyResponse
     );
     VerifyAPI.prototype.fetchStatus = fetchStatus;
 
     // Mock window.location
-    jest.spyOn(window.location, 'assign').mockImplementation( (l:string) => {
-      expect(l).toEqual("https://readid.stepup.example.com/authenticate");
-    })
+    jest.spyOn(window.location, 'assign').mockImplementation((l: string) => {
+      expect(l).toEqual('https://readid.stepup.example.com/authenticate');
+    });
 
     render(
       <CheckSession
@@ -114,20 +105,22 @@ describe("Element should poll verification state", () => {
         retryInSeconds={2}
         maxTries={3}
         timeOutMessage="Unable to process verification"
-      />,
+      />
     );
 
     const tests = [
-      [0, 0], [0, 1],
+      [0, 0],
+      [0, 1],
       [0, 2]
     ];
 
-    expect.assertions(tests.length+1);
+    expect.assertions(tests.length + 1);
 
-    for (let i = 0; i < tests.length; i++) {
+    for (let i = 0; i < tests.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
       await counterTest(tests[i][0], tests[i][1]);
     }
 
     cleanup();
-  })
-})
+  });
+});
--- assets/typescript/authentication/components/qrcode.tsx
+++ assets/typescript/authentication/components/qrcode.tsx
@@ -1,17 +1,14 @@
-import * as React from "react";
-import Image from "react-bootstrap/Image";
-
+import React from 'react';
+import Image from 'react-bootstrap/Image';
 
 export type Props = {
-    image: string
+  image: string;
 };
 
-const QrCode: React.FunctionComponent<Props> = ({ image }) => {
-    return (
-        <div className="m-2 p-2">
-            <Image src={image} thumbnail />
-        </div>
-    )
-}
+const QrCode: React.FC<Props> = ({ image }: Props) => (
+  <div className="m-2 p-2">
+    <Image src={image} thumbnail />
+  </div>
+);
 
 export default QrCode;
